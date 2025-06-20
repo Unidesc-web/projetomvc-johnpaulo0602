af37eb26c3b812171b2747eb2b61c544ae993136
John Paulo <johnpaulo0602@gmail.com>
2025-06-20 20:16:40 -0300

    Configuração inicial do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace projetomvc_johnpaulo0602.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
